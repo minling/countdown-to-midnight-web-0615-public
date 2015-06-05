b05ce0a657763feba955cf0a621a6115a78f0eec
minling <mzhao725@gmail.com>
2015-06-04 22:29:37 -0400

    countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  n = number
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+end
+return "HAPPY NEW YEAR!" 
+  end
+
+def countdown_with_sleep(time)
+  sleep 5
 end
